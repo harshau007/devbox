cc22c93da53b903df672c50ac3fe12583db5da24
harshau007 <amanupadhyay2004@gmail.com>
2024-04-27 19:18:44 +0530

    Modified list command

--- cmd/list.go
+++ cmd/list.go
@@ -6,11 +6,10 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"os"
+	"strings"
+	"time"
 
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-	"github.com/charmbracelet/lipgloss/table"
+	"github.com/docker/docker/api/types"
 	containertypes "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
@@ -28,111 +27,10 @@ type ContainerInfo struct {
 	Url    string
 }
 
-type modelTable struct {
-	table         *table.Table
-	containerList []ContainerInfo
-}
-
-func (m *modelTable) Init() tea.Cmd {
-	return nil
-}
-
-func (m *modelTable) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.table = m.table.Width(msg.Width)
-		m.table = m.table.Height(msg.Height)
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "ctrl+c":
-			return m, tea.Quit
-		case "enter":
-		}
-	}
-	return m, cmd
-}
-
-func (m modelTable) View() string {
-	return "\n" + m.table.String() + "\n(press q to quit)\n"
-}
-
-func (m *modelTable) initTable() {
-	re := lipgloss.NewRenderer(os.Stdout)
-	baseStyle := re.NewStyle().Padding(0, 1)
-	headerStyle := baseStyle.Copy().Foreground(lipgloss.Color("252")).Bold(true)
-	selectedStyle := baseStyle.Copy().Foreground(lipgloss.Color("#01BE85")).Background(lipgloss.Color("#00432F"))
-	typeColors := map[string]lipgloss.Color{
-		"Bug":      lipgloss.Color("#D7FF87"),
-		"Electric": lipgloss.Color("#FDFF90"),
-		"Fire":     lipgloss.Color("#FF7698"),
-		"Flying":   lipgloss.Color("#FF87D7"),
-		"Grass":    lipgloss.Color("#75FBAB"),
-		"Ground":   lipgloss.Color("#FF875F"),
-		"Normal":   lipgloss.Color("#929292"),
-		"Poison":   lipgloss.Color("#7D5AFC"),
-		"Water":    lipgloss.Color("#00E2C7"),
-	}
-	dimTypeColors := map[string]lipgloss.Color{
-		"Bug":      lipgloss.Color("#97AD64"),
-		"Electric": lipgloss.Color("#FCFF5F"),
-		"Fire":     lipgloss.Color("#BA5F75"),
-		"Flying":   lipgloss.Color("#C97AB2"),
-		"Grass":    lipgloss.Color("#59B980"),
-		"Ground":   lipgloss.Color("#C77252"),
-		"Normal":   lipgloss.Color("#727272"),
-		"Poison":   lipgloss.Color("#634BD0"),
-		"Water":    lipgloss.Color("#439F8E"),
-	}
-	columns := []string{
-		"ID",
-		"Name",
-		"Image",
-		"Status",
-		"Volume",
-		"URL",
-	}
-
-	rows := make([][]string, len(m.containerList))
-	for i, container := range m.containerList {
-		rows[i] = []string{container.ID, container.Name, container.Image, container.Status, container.Volume, container.Url}
-	}
-
-	m.table = table.New().Headers(columns...).Rows(rows...).Height(len(m.containerList) + 2).Border(lipgloss.NormalBorder()).
-		BorderStyle(re.NewStyle().Foreground(lipgloss.Color("238"))).
-		StyleFunc(func(row, col int) lipgloss.Style {
-			if row == 0 {
-				return headerStyle
-			}
-
-			if rows[row-1][1] == "Pikachu" {
-				return selectedStyle
-			}
-
-			even := row%2 == 0
-
-			switch col {
-			case 2, 3:
-				c := typeColors
-				if even {
-					c = dimTypeColors
-				}
-
-				color := c[fmt.Sprint(rows[row-1][col])]
-				return baseStyle.Copy().Foreground(color)
-			}
-
-			if even {
-				return baseStyle.Copy().Foreground(lipgloss.Color("252"))
-			}
-			return baseStyle.Copy().Foreground(lipgloss.Color("252"))
-		}).
-		Border(lipgloss.ThickBorder())
-
-}
+var allFlag bool
 
 var listCmd = &cobra.Command{
-	Use:   "list",
+	Use:   "ps",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -141,42 +39,61 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		cont, err := listContainers()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		m := modelTable{containerList: cont}
-		m.initTable()
-		p := tea.NewProgram(&m, tea.WithAltScreen())
-		_, err = p.Run()
-		if err != nil {
-			fmt.Println(err)
+		if allFlag {
+			listAllContainers()
+		} else {
+			listContainers()
 		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+	listCmd.PersistentFlags().BoolVarP(&allFlag, "all", "a", false, "List all containers")
 }
 
-func listContainers() ([]ContainerInfo, error) {
-	containerlist := []ContainerInfo{}
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		return nil, err
-	}
-	defer cli.Close()
+func listContainers() error {
+    ctx := context.Background()
+    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+    if err != nil {
+        return err
+    }
+    defer cli.Close()
 
-	filters := filters.NewArgs(filters.Arg("label", "createdBy=DevControl"))
-	containers, err := cli.ContainerList(ctx, containertypes.ListOptions{All: true, Filters: filters})
-	if err != nil {
-		return nil, err
-	}
+    filters := filters.NewArgs(filters.Arg("label", "createdBy=DevControl"))
+    containers, err := cli.ContainerList(ctx, containertypes.ListOptions{Filters: filters})
+    if err != nil {
+        return err
+    }
 
-	for _, container := range containers {
+    const (
+        containerIDWidth = 12
+        imageWidth       = 20
+        createdWidth     = 20
+        statusWidth      = 20
+        portsWidth       = 20
+        namesWidth       = 20
+    )
+
+	volumeWidth := maxVolumeWidth(containers)
+
+	if len(containers) == 0 {
+        fmt.Println("No containers found.")
+        return nil
+    }
+    fmt.Println(strings.Repeat("-", containerIDWidth+imageWidth+volumeWidth+createdWidth+statusWidth+portsWidth+namesWidth+11))
+
+    fmt.Printf("%-*s\t %-*s %-*s\t %-*s\t %-*s\t %-*s\t %-*s\n",
+        containerIDWidth, "CONTAINER ID",
+        imageWidth, "IMAGE",
+        volumeWidth, "VOLUME",
+        createdWidth, "CREATED",
+        statusWidth, "STATUS",
+        portsWidth, "URL",
+        namesWidth, "NAMES")
+    fmt.Println(strings.Repeat("-", containerIDWidth+imageWidth+volumeWidth+createdWidth+statusWidth+portsWidth+namesWidth+11))
+
+    for _, container := range containers {
 		var url string
 		if len(container.Ports) > 0 {
 			url = fmt.Sprintf("http://%s:%d", container.Ports[0].IP, container.Ports[0].PublicPort)
@@ -184,15 +101,123 @@ func listContainers() ([]ContainerInfo, error) {
 		if url == "" {
 			url = "Not Available"
 		}
-		containerlist = append(containerlist, ContainerInfo{
-			ID:     container.ID[:10],
-			Name:   container.Names[0][1:],
-			Image:  container.Image,
-			Status: container.Status,
-			Url:    url,
-			Volume: container.Mounts[0].Source,
-		})
-	}
+        containerID := truncateString(container.ID, containerIDWidth)
+        image := truncateString(container.Image, imageWidth)
+        volume := truncateString(container.Mounts[0].Source, volumeWidth)
+        created := truncateString(time.Unix(container.Created, 0).Format("2006-01-02 15:04:05"), createdWidth)
+        status := truncateString(container.Status, statusWidth)
+        urls := url
+        names := truncateString(container.Names[0][1:], namesWidth)
 
-	return containerlist, nil
+        fmt.Printf("%-*s\t %-*s %-*s\t %-*s\t %-*s\t %-*s\t %-*s\n",
+            containerIDWidth, containerID,
+            imageWidth, image,
+            volumeWidth, volume,
+            createdWidth, created,
+            statusWidth, status,
+            portsWidth, urls,
+            namesWidth, names,
+        )
+    }
+
+    fmt.Println(strings.Repeat("-", containerIDWidth+imageWidth+volumeWidth+createdWidth+statusWidth+portsWidth+namesWidth+11))
+
+    return nil
+}
+
+func listAllContainers() error {
+    ctx := context.Background()
+    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+    if err != nil {
+        return err
+    }
+    defer cli.Close()
+
+	filters := filters.NewArgs(
+		filters.Arg("label", "createdBy=DevControl"),
+		filters.Arg("status", "exited"),
+	)
+    containers, err := cli.ContainerList(ctx, containertypes.ListOptions{All: true, Filters: filters})
+    if err != nil {
+        return err
+    }
+
+    const (
+        containerIDWidth = 12
+        imageWidth       = 20
+        createdWidth     = 20
+        statusWidth      = 20
+        portsWidth       = 20
+        namesWidth       = 20
+    )
+
+	volumeWidth := maxVolumeWidth(containers)
+
+	if len(containers) == 0 {
+        fmt.Println("No containers found.")
+        return nil
+    }
+
+    fmt.Println(strings.Repeat("-", containerIDWidth+imageWidth+volumeWidth+createdWidth+statusWidth+portsWidth+namesWidth+11))
+
+    fmt.Printf("\t%-*s\t %-*s %-*s\t %-*s\t %-*s\t %-*s\t %-*s\n",
+        containerIDWidth, "CONTAINER ID",
+        imageWidth, "IMAGE",
+        volumeWidth, "VOLUME",
+        createdWidth, "CREATED",
+        statusWidth, "STATUS",
+        portsWidth, "URL",
+        namesWidth, "NAMES")
+    fmt.Println(strings.Repeat("-", containerIDWidth+imageWidth+volumeWidth+createdWidth+statusWidth+portsWidth+namesWidth+11))
+
+    for _, container := range containers {
+		var url string
+		if len(container.Ports) > 0 {
+			url = fmt.Sprintf("http://%s:%d", container.Ports[0].IP, container.Ports[0].PublicPort)
+		}
+		if url == "" {
+			url = "Not Available"
+		}
+        containerID := truncateString(container.ID, containerIDWidth)
+        image := truncateString(container.Image, imageWidth)
+        volume := truncateString(container.Mounts[0].Source, volumeWidth)
+        created := truncateString(time.Unix(container.Created, 0).Format("2006-01-02 15:04:05"), createdWidth)
+        status := truncateString(container.Status, statusWidth)
+        urls := url
+        names := truncateString(container.Names[0][1:], namesWidth)
+
+        fmt.Printf("\t%-*s\t %-*s %-*s\t %-*s\t %-*s\t %-*s\t %-*s\n",
+            containerIDWidth, containerID,
+            imageWidth, image,
+            volumeWidth, volume,
+            createdWidth, created,
+            statusWidth, status,
+            portsWidth, urls,
+            namesWidth, names,
+        )
+    }
+
+    fmt.Println(strings.Repeat("-", containerIDWidth+imageWidth+volumeWidth+createdWidth+statusWidth+portsWidth+namesWidth+11))
+
+    return nil
+}
+
+func maxVolumeWidth(containers []types.Container) int {
+    maxWidth := 0
+    for _, container := range containers {
+        if len(container.Mounts) > 0 {
+            volumePath := container.Mounts[0].Source
+            if len(volumePath) > maxWidth {
+                maxWidth = len(volumePath)
+            }
+        }
+    }
+    return maxWidth
+}
+
+func truncateString(str string, width int) string {
+    if len(str) > width {
+        return str[:width]
+    }
+    return str
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.22.2
 require (
 	github.com/briandowns/spinner v1.23.0
 	github.com/charmbracelet/bubbletea v0.25.0
-	github.com/charmbracelet/lipgloss v0.9.1
 	github.com/docker/docker v26.0.1+incompatible
 	github.com/spf13/cobra v1.8.0
 )
